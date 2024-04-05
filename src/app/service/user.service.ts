959f2d8c50f28be2b1423f04016ab95f26b9d606
andreyherrera <andreyherrerac@icloud.com>
2024-04-04 23:03:37 -0500

    Add user service methods for registration, login, making/getting/canceling reservations, and session validation; inject HttpClient

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environment/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private readonly httpClient: HttpClient) {}
 
-  constructor() { }
+  registerUser(userData: any) {
+    return this.httpClient.post(environment.apiUserRegiser, userData);
+  }
+
+  loginUser(userData: any) {
+    return this.httpClient.post(environment.apiUserLogin, userData);
+  }
+
+  validateSession(userData: any) {
+    return this.httpClient.post(environment.apiUserValidate, userData);
+  }
+
+  makeReservation(reservationData: any) {
+    return this.httpClient.post(
+      environment.apiReservationMake,
+      reservationData
+    );
+  }
+
+  getReservation(reservationData: any) {
+    return this.httpClient.post(environment.apiReservationGet, reservationData);
+  }
+
+  cancelReservation(reservationData: any) {
+    return this.httpClient.post(
+      environment.apiReservationCancel,
+      reservationData
+    );
+  }
 }
