82ea251f9306a37f109a06588621eb88b5213dee
andreyherrera <andreyherrerac@icloud.com>
2024-04-04 22:36:28 -0500

    Add User and Reservation components and update Angular and dependency versions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: UserComponent },
+  { path: 'reservation', component: ReservationComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
