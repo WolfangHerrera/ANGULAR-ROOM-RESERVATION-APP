8017220c88e66444752d75079f6a16dc35bf93bc
andreyherrera <andreyherrerac@icloud.com>
2024-04-07 17:15:07 -0500

    "Added HeaderComponent to AppModule declarations and updated ReservationComponent HTML to include HeaderComponent"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { UserComponent } from './user/user.component';
 import { ReservationComponent } from './reservation/reservation.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './shared/header/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, UserComponent, ReservationComponent],
+  declarations: [AppComponent, UserComponent, ReservationComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
